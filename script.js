5f6fd4bf6751dbbcb6f58a7b367305dc39868617
colin11702 <colinmcherney@gmail.com>
2023-07-29 19:16:56 -0700

    Added functional about button

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+function about() {
+  $(".right-side").empty();
+  $(".right-side").append("<h2>Location:</h2>");
+  $(".right-side").append("<h3>Scottsdale, Arizona</h3>");
+  $(".right-side").append("<br />");
+  $(".right-side").append("<h2>Education:</h2>");
+  $(".right-side").append("<h3>Arizona State University</h3>");
+  $(".right-side").append("<h3>Computer Information Systems, BS (2024)</h3>");
+  $(".right-side").append("<br />");
+  $(".right-side").append("<h2>About Me:</h2>");
+  $(".right-side").append(
+    "<h3>Hello ! I'm Colin, a passionate and curious student developer with an insatiable appetite for all things related to technology. I believe that technology is not just a tool but a medium to bring innovative ideas to life and make a positive impact on the world.</h3>"
+  );
+  $(".right-side").append("<br />");
+  $(".right-side").append("<h2>Future Goals:</h2>");
+  $(".right-side").append(
+    "<h3>As I progress in my academic journey, my ultimate goal is to make a meaningful contribution to the technology industry. I aspire to work on projects that tackle real-world problems, leveraging technology to improve people's lives and transform the way we interact with the world around us.</h3>"
+  );
+}
+
 function experience() {
   $(".right-side").empty();
 }
