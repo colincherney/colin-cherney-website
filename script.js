a27ca90f1f446e21d7b7dc98951c317f224f77e0
colin11702 <colinmcherney@gmail.com>
2023-07-29 20:05:13 -0700

    Added Projects

--- script.js
+++ script.js
@@ -20,4 +20,90 @@ function about() {
 
 function experience() {
   $(".right-side").empty();
+  $(".right-side").append("<h2>Technologies I Have Worked With:</h2>");
+  $(".right-side").append("<img class='icons' src='icons/python.png' />");
+  $(".right-side").append("<img class='icons' src='icons/java.png' />");
+  $(".right-side").append("<img class='icons' src='icons/html.png' />");
+  $(".right-side").append("<img class='icons' src='icons/css.png' />");
+  $(".right-side").append("<img class='icons' src='icons/javascript.png' />");
+  $(".right-side").append("<img class='icons' src='icons/jquery.png' />");
+  $(".right-side").append("<img class='icons' src='icons/node.png' />");
+  $(".right-side").append("<img class='icons' src='icons/mysql.png' />");
+  $(".right-side").append("<img class='icons' src='icons/git.png' />");
+  $(".right-side").append("<img class='icons' src='icons/github.png' />");
+  $(".right-side").append("<img class='icons' src='icons/vs-code.png' />");
 }
+
+function projects() {
+  $(".right-side").empty();
+
+  // weather project
+  $(".right-side").append("<img class='project-img' src='weather.gif' />");
+  $(".right-side").append("<h2 class='project-title'>Weather Site</h2>");
+  $(".right-side").append("<div class='project-icons'></div>");
+  $(".project-icons").append("<img src='icons/html.png' />");
+  $(".project-icons").append("<img src='icons/css.png' />");
+  $(".project-icons").append("<img src='icons/javascript.png' />");
+  $(".project-icons").append("<img src='icons/jquery.png' />");
+  $(".right-side").append("<div class='demo-and-source'></div>");
+  $(".demo-and-source").append(
+    "<a href='https://colincherney.github.io/weather-app-v2/' target='_blank' class='demo-and-source-buttons'>Live Demo</a>"
+  );
+  $(".demo-and-source").append(
+    "<a href='https://github.com/colincherney/weather-app-v2' target='_blank' class='demo-and-source-buttons'>Source Code</a>"
+  );
+  $(".right-side").append("<div class='seperate-projects'></div>");
+
+  // etch-a-sketch project
+  $(".right-side").append(
+    "<img class='project-img' src='etch-a-sketch.gif' />"
+  );
+  $(".right-side").append("<h2 class='project-title'>Etch-A-Sketch</h2>");
+  $(".right-side").append("<div class='project-icons2'></div>");
+  $(".project-icons2").append("<img src='icons/html.png' />");
+  $(".project-icons2").append("<img src='icons/css.png' />");
+  $(".project-icons2").append("<img src='icons/javascript.png' />");
+  $(".right-side").append("<div class='demo-and-source2'></div>");
+  $(".demo-and-source2").append(
+    "<a href='https://colincherney.github.io/Etch-a-Sketch/' target='_blank' class='demo-and-source-buttons'>Live Demo</a>"
+  );
+  $(".demo-and-source2").append(
+    "<a href='https://github.com/colincherney/Etch-a-Sketch' target='_blank' class='demo-and-source-buttons'>Source Code</a>"
+  );
+  $(".right-side").append("<div class='seperate-projects'></div>");
+
+  // rock paper scissors project
+  $(".right-side").append(
+    "<img class='project-img' src='rock-paper-scissors.gif' />"
+  );
+  $(".right-side").append("<h2 class='project-title'>Rock Paper Scissors</h2>");
+  $(".right-side").append("<div class='project-icons3'></div>");
+  $(".project-icons3").append("<img src='icons/html.png' />");
+  $(".project-icons3").append("<img src='icons/css.png' />");
+  $(".project-icons3").append("<img src='icons/javascript.png' />");
+  $(".right-side").append("<div class='demo-and-source3'></div>");
+  $(".demo-and-source3").append(
+    "<a href='https://colincherney.github.io/rock-paper-scissors/' target='_blank' class='demo-and-source-buttons'>Live Demo</a>"
+  );
+  $(".demo-and-source3").append(
+    "<a href='https://github.com/colincherney/rock-paper-scissors' target='_blank' class='demo-and-source-buttons'>Source Code</a>"
+  );
+  $(".right-side").append("<div class='seperate-projects'></div>");
+
+  // calculator project
+  $(".right-side").append("<img class='project-img' src='calculator.gif' />");
+  $(".right-side").append("<h2 class='project-title'>Calculator</h2>");
+  $(".right-side").append("<div class='project-icons4'></div>");
+  $(".project-icons4").append("<img src='icons/html.png' />");
+  $(".project-icons4").append("<img src='icons/css.png' />");
+  $(".project-icons4").append("<img src='icons/javascript.png' />");
+  $(".right-side").append("<div class='demo-and-source4'></div>");
+  $(".demo-and-source4").append(
+    "<a href='https://colincherney.github.io/calculator/' target='_blank' class='demo-and-source-buttons'>Live Demo</a>"
+  );
+  $(".demo-and-source4").append(
+    "<a href='https://github.com/colincherney/calculator' target='_blank' class='demo-and-source-buttons'>Source Code</a>"
+  );
+}
+
+onload = about();
